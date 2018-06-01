527a069d636a55ed9a5a07969bdefe980c172731
Alex Oancea <alx.oancea@gmail.com>
2018-06-01 20:11:04 +0300

    add routing and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes = [
+    { path: 'home', component: HomeComponent },
+
+    { path: '', redirectTo: '/home', pathMatch: "full" }
+]
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        RouterModule.forRoot(routes)
     ],
     providers: [],
     bootstrap: [AppComponent]
