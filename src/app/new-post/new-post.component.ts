23e0e3e0c631c36adfb7672774e42e18ab076334
Alex Oancea <alx.oancea@gmail.com>
2018-06-03 18:32:54 +0300

    clean old contract address

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -30,8 +30,7 @@ export class NewPostComponent implements OnInit {
         var HashStoreABI = [{ "constant": false, "inputs": [{ "name": "content", "type": "string" }], "name": "save", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "constant": true, "inputs": [{ "name": "_hashId", "type": "uint256" }], "name": "find", "outputs": [{ "name": "hashSender", "type": "address" }, { "name": "hashContent", "type": "string" }, { "name": "hashTimestamp", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "hashes", "outputs": [{ "name": "sender", "type": "address" }, { "name": "content", "type": "string" }, { "name": "timestamp", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "lastHashId", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }];
 
         var HashStoreContract = this.web3Provider.web3.eth.contract(HashStoreABI);
-
-        //this.hashStoreInstance = HashStoreContract.at('0xc6abe135d67fe7e86eae72658d6c9e33f949a9d5');
+        
         this.hashStoreInstance = HashStoreContract.at('0x0510ad11b347b84c0c7cd83b18af0a3147e76883');
     }
 
