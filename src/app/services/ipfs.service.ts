49269cb882c39258c0ba36baf13e058f986dfe74
Alex Oancea <alx.oancea@gmail.com>
2018-06-03 21:49:16 +0300

    switched ipfs to infura node for ipfs deploy

--- src/app/services/ipfs.service.ts
+++ src/app/services/ipfs.service.ts
@@ -8,8 +8,8 @@ export class IpfsService {
     public ipfs: any;
 
     constructor() {
-        this.ipfs = new IPFS({ host: 'localhost', port: 5001, protocol: 'http' });
+        //this.ipfs = new IPFS({ host: 'localhost', port: 5001, protocol: 'http' });
 
-        //this.ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+        this.ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
     }
 }
