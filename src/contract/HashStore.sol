44a2327f0ce257849484d1a69989249e8b8c126c
Alex Oancea <alx.oancea@gmail.com>
2018-06-04 19:56:11 +0300

    manage profile, update post from contract

--- src/contract/HashStore.sol
+++ src/contract/HashStore.sol
@@ -2,6 +2,9 @@ pragma solidity ^0.4.11;
 
 contract HashStore {
 
+    mapping(address => string) private profiles;
+
+
     struct Hash {
         address sender;
         string content;
@@ -25,5 +28,26 @@ contract HashStore {
 
     function find(uint _hashId) view public returns (address hashSender, string hashContent, uint hashTimestamp) {
         return (hashes[_hashId].sender, hashes[_hashId].content, hashes[_hashId].timestamp);
+    }
+
+    function post_update(uint hashId, string content) public {
+        Hash storage hash = hashes[hashId];
+
+        require(hash.sender == msg.sender);
+
+        hash.content = content;
+        hash.timestamp = block.timestamp;
+    }
+
+
+
+
+
+    function profile_save(string profile_hash) public {
+        profiles[msg.sender] = profile_hash;
+    }
+
+    function profile_get() view public returns (string profile_hash) {
+        return (profiles[msg.sender]);
     }
 }
