c1e1b1fa761eb60506505cdc96eef7f278fcf0f9
Alex Oancea <alx.oancea@gmail.com>
2018-06-05 08:25:35 +0300

    add prod variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+    production: true,
+
+    hash_store_contract_address_local: '0x0510ad11b347b84c0c7cd83b18af0a3147e76883',
+    hash_store_contract_address_ropsten: '0xe2449AdEbB811a7253D3f5D0ADD86283728a6F56',
 };
