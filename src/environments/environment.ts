251672294bf5bc35cef9344a01106a1b517fb617
Alex Oancea <alx.oancea@gmail.com>
2018-06-05 08:21:39 +0300

    new deployment on ropsten

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     production: false,
 
     hash_store_contract_address_local: '0x0510ad11b347b84c0c7cd83b18af0a3147e76883',
-    hash_store_contract_address_ropsten: '0x32d07cbd2ecbeddfc7908f6fdcae99a2fddbe18b',
+    hash_store_contract_address_ropsten: '0xe2449AdEbB811a7253D3f5D0ADD86283728a6F56',
 };
 
 /*
